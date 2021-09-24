6ea5b23acb52af9756587a078e3ff12aa92e42c2
dgoalmeida <dgoalmeida@gmail.com>
2021-09-24 10:11:19 -0300

    adicionando scripts R

--- R/classificacao-risco-de-credito.R
+++ R/classificacao-risco-de-credito.R
@@ -17,7 +17,7 @@
 # https://archive.ics.uci.edu/ml/datasets/Statlog+(German+Credit+Data)
 ################################################################################
 
-setwd('../../../../data/')
+# setwd('../../../../data/')
 
 # realizar atividade de engenharia de atributos
 # atividade de quantizar algumas variaveis (converter de numérica para categorica)
@@ -38,11 +38,30 @@ if(Azure){
   #library(labelled)
   
   # alterando nome das colunas
-  colnames(Credit) =  c('StatusChecking', 'Duration', 'HistoryCredit', 'Purpose', 'CreditAmount', 'SavingAccount', 'EmploymentSince', 'InstallmentRate', 'PersonalStatus', 'Guarantor', 'ResidenceSince', 'Property', 'Age', 'InstallmentPlans', 'housing', 'CreditsAtBank', 'Job', 'maintenance', 'Phone', 'ForeignWorker', 'CreditStatus')
-  
+  colnames(Credit) =  c("CheckingAcctStat",
+                        "Duration",
+                        "CreditHistory",
+                        "Purpose",
+                        "CreditAmount",
+                        "SavingsBonds",
+                        "Employment",
+                        "InstallmentRatePecnt",
+                        "SexAndStatus",
+                        "OtherDetorsGuarantors",
+                        "PresentResidenceTime",
+                        "Property",
+                        "Age",
+                        "OtherInstalments",
+                        "Housing",
+                        "ExistingCreditsAtBank",
+                        "Job",
+                        "NumberDependents",
+                        "Telephone",
+                        "ForeignWorker", "CreditStatus")
   #adicionando label as colunas
   var_label(Credit) = c('StatusChecking', 'Duration', 'HistoryCredit', 'Purpose', 'CreditAmount', 'SavingAccount', 'EmploymentSince', 'InstallmentRate', 'PersonalStatus', 'Guarantor', 'ResidenceSince', 'Property', 'Age', 'InstallmentPlans', 'housing', 'CreditsAtBank', 'Job', 'maintenance', 'Phone', 'ForeignWorker', 'CreditStatus')
   
+  View(Credit)
 }
 
 # transformando variaveis numéricas em variáveis categoricas
@@ -51,4 +70,39 @@ maxVals = c(100, 1000000, 100)
 fac_Names = unlist(lapply(toFactors, function(x) paste(x, "_f", sep="")))
 Credit[,fac_Names] = Map(function(x,y) quantize.num(Credit[, x], maxval = y), toFactors, maxVals)
 
-View(Credit)
+if(Azure) {
+  maml.mapOutputPort('Credit')
+} 
+
+#######################
+#######################
+# Analise exploratória
+
+if(Azure){
+  source("src/ClassTools.R")
+  Credit = maml.mapInputPort(1)
+}else{
+  source("src/ClassTools.R")
+}
+  
+# criando plots usando ggplot2
+
+library(ggplot2)
+lapply(colNames2, function(x){
+  if(is.factor(Credit[,x])){
+    ggplot(Credit, aes_string(x)) +
+      geom_bar() +
+      facet_grid(. ~ CreditStatus) +
+      ggtitle(paste("Total de Crédito bom/Ruim por ", x))
+  }
+})
+
+# Plot CreditStatus vs StatusChecking
+lapply(colNames2, function(x){
+  if(is.factor(Credit[,x]) & x != "StatusChecking") {
+    ggplot(Credit, aes(StatusChecking)) +
+      geom_bar() +
+      facet_grid(paste(x, " ~ CreditStatus")) +
+      ggtitle(paste("Total de Crédito bom/Ruim StatusChecking e ", x))
+  }
+})
