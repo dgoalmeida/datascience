79969542d6e87d952dfda5b1f1db46241a9d01ca
dgoalmeida <dgoalmeida@gmail.com>
2021-09-28 16:04:24 -0300

    adicionando scripts R

--- R/classificacao-risco-de-credito.R
+++ R/classificacao-risco-de-credito.R
@@ -153,3 +153,100 @@ modelo = randomForest(
 varImpPlot(modelo)
 
 outFrame - serList(list(Credit.model = modelo))
+
+########################################################################
+# criando modelo preditivo
+
+prop.table(table(Credit$CreditStatus))
+
+# função para gerar dados de treino e de test
+splitData = function(dataframe, seed = NULL){
+  if(!is.null(seed)) set.seed(seed)
+  index = 1:nrow(dataframe)
+  trinindex = sample(index, trunc(length(index)/2))
+  trainset = dataframe[trinindex,]
+  testset = dataframe[-trinindex,]
+  list(trainset = trainset, testset = testset)
+}
+
+# gerando dados de treino e test
+split = splitData(Credit, seed = 808)
+
+#separando dados
+dados_treino = split$trainset
+dados_test = split$testset
+
+# verificando numero de linhas
+nrow(dados_treino)
+nrow(dados_test)
+
+dados_treino$CreditStatus = as.factor(dados_treino$CreditStatus)
+str(dados_treino$CreditStatus)
+
+# construindo o modelo
+library(randomForest)
+
+modelo = randomForest(CreditStatus ~ CheckingAcctStat
+                        + Duration_f
+                        + Purpose
+                        + SavingsBonds
+                        + Employment
+                        + CreditAmount_f ,
+             data = dados_treino,
+             ntree = 100, 
+             nodesize = 10)
+
+print(modelo)
+
+################################################################################
+################################################################################
+# Avaliação da performance do modelo
+
+previsoes = data.frame(observado = dados_test$CreditStatus,
+                          previsto = predict(modelo, newData =  dados_test))
+
+previsoes$result = previsoes$observado == previsoes$previsto
+
+# visualizando resultado
+View(previsoes)
+View(dados_test)
+
+################################################################################
+################################################################################
+# calculando as metricas manualmente para analisar a performance do modelo.
+
+Acuracy = function(x) {
+  (x[1,1] + x[2,2]) / (x[1,1] + x[1,2] + x[2,1] + x[2,2])
+}
+
+Recall = function(x) {
+  x[1,1] / (x[1,1] + x[1,2])
+}
+
+Presicion = function(x) {
+  x[1,1] / (x[1,1] + x[2,1])
+}
+
+W_Accuracy = function(x) {
+  (x[1,1] + x[2,2]) / (x[1,1] + 5 * x[1,2] + x[2,1] + x[2,2])
+}
+
+F1 = function(x){
+  2 * x[1,1] / ( 2 * x[1,1] + x[1,2] + x[2,1])
+}
+
+# gerando a matriz de confusao manualmente
+
+confusion_matrix = matrix(unlist(Map(function(x, y){sum(ifelse(previsoes[,1] == x & previsoes[,2] == y, 1, 0))},
+                                     c(2,1,2,1), c(2,2,1,1))), nrow = 2)
+
+confusion_matrix
+
+# criando um dataframe com as estatisticas
+df_mat = data.frame(Category = c("Credito Ruim", "Credito bom"),
+                    Classificado_como_ruim = c(confusion_matrix[1,1], confusion_matrix[2,1]),
+                    Classificado_como_bom = c(confusion_matrix[1,2], confusion_matrix[2,2]),
+                    Accuracy_Recall = c(Acuracy(confusion_matrix), Recall(confusion_matrix)),
+                    Precision_Wacc = c(Presicion(confusion_matrix), W_Accuracy(confusion_matrix)))
+
+print(df_mat)
